6ae76192993a0883453a98784f45aa9ba1803227
Marina Barbosa <marinabspanenberg@gmail.com>
2024-11-24 15:47:31 -0300

    fix - Adicionado funcionalidades básicas da página 'Favoritos' e seus estilos em 'FavoritosStyle'

--- src/pages/Favoritos.js
+++ src/pages/Favoritos.js
@@ -1,35 +1,46 @@
 import { useNavigation } from '@react-navigation/native';
-import React from 'react'
-import { Image } from 'react-native';
+import React, { useState } from 'react'
+import { Image, Text, TouchableOpacity, View } from 'react-native';
 import favoritosIcone from '../assets/images/favoritosIcone.png'
+import BotaoVoltarOuSairRB from '../components/BotaoVoltarOuSairRB';
+import FavoritosStyles from './styles/FavoritosStyles';
 
 export default function Favoritos() {
-  const navigation = useNavigation();
-    
-  return (
-    <View style={DetalhesParadaStyles.containerGeral}>
+  const navigation = useNavigation ();
+  const [activeButton, setActiveButton] = useState(null); // Estado para rastrear o botão ativo
 
-        <View style={DetalhesParadaStyles.containerTopo}>
+  const handlePress = (buttonId) => {
+    setActiveButton(buttonId); // Define o botão pressionado como ativo
+  };
+
+  return (
+    <View style={FavoritosStyles.containerGeral}>
+
+        <View style={FavoritosStyles.containerTopo}>
 
             <BotaoVoltarOuSairRB acao={() => navigation.goBack()} />
             
-            <View style={DetalhesParadaStyles.logoContainer}>
-                <Image source={favoritosIcone}/>
+            <View style={FavoritosStyles.estrelaContainer}>
+                <Image source={favoritosIcone} style={FavoritosStyles.estrelaImagem}/>
             </View>
-
-            <View style={DetalhesParadaStyles.nomeDaParadaContainer}>
-                <Text style={DetalhesParadaStyles.textoNomeDaParada}>Nome da parada</Text>
-            </View>
-
         </View>
-        <View style={DetalhesParadaStyles.containerBase}>
-            <View style={DetalhesParadaStyles.onibusImagemContainer}>
-               <Image source={onibusIcone} style={DetalhesParadaStyles.onibusImagem}></Image>
-            </View>
-            <View style={DetalhesParadaStyles.relogioImagemContainer}>
-               <Image source={relogioIcone} style={DetalhesParadaStyles.relogioImagem}></Image>
-            </View>
-
+        
+        <View style={FavoritosStyles.containerBase}>
+            <View style={FavoritosStyles.buttonContainer}>
+                <TouchableOpacity
+                    style={[FavoritosStyles.button, activeButton === 1 && FavoritosStyles.buttonPressionado]}
+                    onPress={() => handlePress(1)}
+                    >
+                        <Text 
+                            style={[FavoritosStyles.buttonText, activeButton === 1 && FavoritosStyles.buttonPressionadoTexto ]}>PARADAS</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={[FavoritosStyles.button, activeButton === 2 && FavoritosStyles.buttonPressionado ]} 
+                    onPress={() => handlePress(2)} >
+                        <Text 
+                            style={[FavoritosStyles.buttonText, activeButton === 2 && FavoritosStyles.buttonPressionadoTexto ]}>ÔNIBUS</Text>
+                </TouchableOpacity>
+          </View>
         </View>
     </View>    
   )
