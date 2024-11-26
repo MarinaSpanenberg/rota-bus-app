9e1253f5533ba4a788daa29ae43682a57be50715
Marina Barbosa <marinabspanenberg@gmail.com>
2024-11-26 15:06:50 -0300

    fix - Funcionalidade da página 'RemoverOnibus' está funcional

--- src/pages/styles/RemoverOnibusStyle.js
+++ src/pages/styles/RemoverOnibusStyle.js
@@ -8,6 +8,17 @@ export default StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
     },
+    tituloContainer: {
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        top: 100
+    },
+    tituloTexto: {
+        color: cores.branco,
+        fontSize: tamanhoFontes.media,
+        fontFamily: 'PoppinsExtraBold'
+    },
     containerTopo: {
         width: '100%',
         height: '25%',
@@ -16,6 +27,22 @@ export default StyleSheet.create({
     containerBase: {
         width: '100%',
         height: '75%',
+        flexDirection: 'row',
         backgroundColor: cores.branco,
     },
+    favoritoItem:{
+        width: '100%',
+        alignItems: 'flex-start',
+        padding: 10,
+        borderBottomWidth: 1,
+        borderStyle: 'dotted',
+        borderColor: cores.cinza,
+    },
+    deletarBotaoContainer: {
+        width: '100%',
+    },
+    deletarBotao: {
+        height: 20,
+        resizeMode: 'contain' 
+    }
 });
